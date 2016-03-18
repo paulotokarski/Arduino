afd6ec3b7b37333ab740441ced93cc2397532930
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2016-03-17 23:46:28 -0300

    Added files via upload

---
+++ blink_led_ldr/blink_led_ldr.ino
@@ -0,0 +1,22 @@
+#define LED 10
+#define LDR 5
+
+int light = 0;
+
+void setup() {
+  pinMode(LDR, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  light = analogRead(5);
+
+  if(light < 100)
+    digitalWrite(LED, HIGH);
+  else
+    digitalWrite(LED, LOW);
+
+  Serial.println(light);
+  
+}
